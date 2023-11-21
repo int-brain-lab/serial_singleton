038c8ccf3c8dbf7ffc83f5e366e845ecdf36cb8d
Florian Rau <bimac@users.noreply.github.com>
2023-11-21 10:37:04 +0000

    Update test_serial_singleton.py

--- tests/test_serial_singleton.py
+++ tests/test_serial_singleton.py
@@ -13,31 +13,27 @@ class Child2(SerialSingleton):
 
 class TestSingleton(unittest.TestCase):
     def test_unconnected(self):
-        ser = Child1(connect=False)
+        # creation of an instance without opening the connection
+        ser = Child1(port='FakePort', connect=False)
         assert ser.is_open is False
 
     def test_singleton(self):
         # create a few instances of Child1
-        ser1 = Child1(connect=False)
-        ser2 = Child1(connect=False)
-        ser3 = Child1('FakePort3', connect=False)
-        ser4 = Child1('FakePort4', connect=False)
-        ser5 = Child1(port='FakePort4', connect=False)
-
-        # assert that a port blocked by *another* SerialSingleton child can't be used
-        with self.assertRaises(SerialSingletonException):
-            Child2('FakePort4')
-
-        # assert that port cannot be changed after initialization
-        with self.assertRaises(SerialSingletonException):
-            ser5.port = 'some_other_port'
+        ser1 = Child1(port='FakePort1', connect=False)
+        ser2 = Child1(port='FakePort1', connect=False)
+        ser3 = Child1(port='FakePort2', connect=False)
 
         # assert singleton behavior
         assert ser1 is ser2
         assert ser1 is not ser3
-        assert ser3 is not ser4
-        assert ser4 is ser5
 
-    def test_set_port(self):
-        ser = Child1(connect=False)
-        self.assertRaises(Exception, ser.setPort)
+        # assert that a port blocked by Child1 can't be used by Child2
+        with self.assertRaises(SerialSingletonException):
+            Child2(port=ser1.port)
+
+        # assert that port cannot be changed after initialization
+        with self.assertRaises(SerialSingletonException):
+            ser3.port = 'FakePort3'
+
+        # assert that port cannot be changed after instantiation
+        self.assertRaises(Exception, ser1.setPort)
